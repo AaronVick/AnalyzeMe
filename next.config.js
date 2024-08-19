660a2d5191da4510e880a2af8c775e9dd2e9b260
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-08-18 21:13:58 -0500

    Ensure all changes are committed and pushed

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['analyze-me.vercel.app'],
-  },
-  // Comment out the custom Webpack configuration for now
-  // webpack: (config) => {
-  //   config.externals.push({
-  //     sharp: "commonjs sharp",
-  //   });
-  //   return config;
-  // },
 }
 
 module.exports = nextConfig;
